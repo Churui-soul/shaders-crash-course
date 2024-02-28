badf8f50d57171c5c06abae6575be7a7fbdbe2ba
cn5508@163.com <www574442497==>
2024-02-28 15:39:47 +0800

    同步

---
+++ src/shaders/fragment.glsl
@@ -0,0 +1,11 @@
+void main() {
+    // abs
+    // min, max
+    // mod 
+    // sin, cos, tan, atan
+    // dot, cross
+    // clamp(min, max) - 可以限制最大最小值
+
+
+    gl_FragColor = vec4(mod(9.0, 4.25), 0.0, 0.0, 1.0);
+}
